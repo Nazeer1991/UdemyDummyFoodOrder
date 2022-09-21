1b71d08714d734b61f12715a37f9c6802ffb59b9
nazeersathak <nazeersathak@gmail.com>
2022-09-21 15:27:09 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import { useState } from "react";
-import Cart from "./components/Cart/Cart";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
   return (
-    <>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
