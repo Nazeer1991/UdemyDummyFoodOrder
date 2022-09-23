e124694a494e92fbd1d89a21beb7062548799051
nazeersathak <nazeersathak@gmail.com>
2022-09-23 18:47:28 +0530

    finished Cart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
-import CartProvider from "./store/cartProvider";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setSetCartIsShown] = useState(false);
